5e446a7f3f4b646aa21310b0d28a7cd15c89056b
hsnassaad <hassanassaad15@gmail.com>
2020-03-07 14:10:18 +0200

    Remove Identity

--- OldHouse/Data/ApplicationDbContext.cs
+++ OldHouse/Data/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ using System;
 
 namespace OldHouse.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<Patient>
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
